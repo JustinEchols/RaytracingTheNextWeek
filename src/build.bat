9aeed2b138412776f993f33172395d4ee27b2682
Justin Echols <justindawsonechols@gmail.com>
2023-12-12 20:53:08 -0600

    Set warning level to -Wall

--- src/build.bat
+++ src/build.bat
@@ -4,7 +4,7 @@ IF NOT EXIST ..\build mkdir ..\build
 pushd ..\build
 
 set main_file=raytracer.cpp
-set common_compiler_flags=-Od -Zi -EHsc
+set common_compiler_flags=-Od -Zi -EHsc -Wall -wd4820 -wd4640 -wd4350 -wd4710 -wd4514 -wd4625 -wd4571 -wd4265 -wd4626 -wd4100
 set include_dir= /I "../src"
 
 cl ..\src\%main_file% %common_compiler_flags% %include_dir%
